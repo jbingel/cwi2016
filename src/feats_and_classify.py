1778360ba784266a147fe2eae66941571cc86736
alonso <hector.martinez.a@gmail.com>
2016-01-22 15:57:14 +0100

    huh

--- src/feats_and_classify.py
+++ src/feats_and_classify.py
@@ -226,7 +226,7 @@ def collect_labels(cwi_file, annotatorIdx):
         labels.append(int(line.split('\t')[3+annotatorIdx]))
     return np.array(labels)
 
-def collect_features(data,vectorize=True):
+def collect_features(data,vectorize=True,generateFeatures=True):
     labels = []
     featuredicts = []
     
@@ -238,7 +238,8 @@ def collect_features(data,vectorize=True):
        for l,i in zip(s["label"],s["idx"]):
             if l != "-":
                 w = WordInContext(s, i, s["form"][i],s["lemma"][i],s["pos"][i],s["ne"][i],l,s["head"],s["deprel"])
-                featuredicts.append(w.featurize())
+                if generateFeatures:
+                    featuredicts.append(w.featurize())
                 labels.append(w.label)
     print()
     if vectorize:
